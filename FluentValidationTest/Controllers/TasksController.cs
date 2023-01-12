f99183086cbcba5e890577eb2a077962e2ef45d4
Mustafa Hakan Orskaya <mhorskaya@hotmail.com>
2023-01-12 13:38:50 +0300

    Add tasks controller

---
+++ FluentValidationTest/Controllers/TasksController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FluentValidationTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TasksController : ControllerBase
+    {
+        private readonly ILogger<TasksController> _logger;
+
+        public TasksController(ILogger<TasksController> logger) => _logger = logger;
+
+        [HttpPost]
+        public TaskItem Post(TaskItem model)
+        {
+            // Do something...
+
+            return model;
+        }
+    }
+}
---
+++ FluentValidationTest/TaskItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FluentValidationTest
+{
+    public class TaskItem
+    {
+        public string Description { get; set; } = null!;
+
+        public DateTime DueDate { get; set; }
+
+        public bool RemindMe { get; set; }
+
+        public int? ReminderMinutesBeforeDueDate { get; set; }
+
+        public List<string> SubItems { get; set; }
+    }
+}
